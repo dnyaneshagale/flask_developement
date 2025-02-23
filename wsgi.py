5af9ad1ec33171b52b825294a0bfe890d03cb18b
Dnyanesh Agale <150069504+dnyaneshagale@users.noreply.github.com>
2025-02-23 09:13:19 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from your_flask_app import app  # Replace with your actual Flask app module
+from app import app  # Replace with your actual Flask app module
 
 if __name__ == "__main__":
     app.run()
